539ff303bd753969ad5d5beb164da0090e4fc7b7
Spongey21 <driton.latifi101@gmail.com>
2023-04-18 22:44:14 +0200

    .

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-import * as pg from './script.js'
+import * as floof from './script.js'
 
 const IMAGE = document.querySelector('.slider img');
 const POINTER = document.querySelector('.pointer');
 
-pg.default.REQUEST('https://api.themoviedb.org/3/movie/popular?api_key=d61d03c4897622853f09d1e0b7a41c5b&page=1', IMAGE, POINTER)
+floof.default.REQUEST('https://api.themoviedb.org/3/movie/popular?api_key=d61d03c4897622853f09d1e0b7a41c5b&page=1', IMAGE, POINTER)
